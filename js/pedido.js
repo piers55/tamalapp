a94743dfe4816ec88fed1e28d2c6e9366401ed48
Miguel Segura <zurol@pcuervo.com>
2014-09-29 13:28:25 -0500

    Funciones para localización en el mapa

--- js/pedido.js
+++ js/pedido.js
@@ -195,6 +195,146 @@
 			}
 			visualizar();
 		}
+//___________________________________________________________________________________
 
 
-		
+		//(<alt>, y, x, z-index)
+      var beaches = [
+        ['Juan', 19.415, -99.170, 'fijo', 0, 7, 3, 4, 5],
+        ['Pedro', 19.41, -99.170, 'fijo', 1, 2, 0, 7, 5],
+        ['Adolfo', 19.405,  -99.170, 'movil', 1, 2, 3, 4, 5],
+        ['Carlos', 19.405,  -99.175, 'movil', 1, 7, 3, 0, 5],
+        ['Gerardo', 19.41,  -99.175, 'fijo', 1, 0, 3, 4, 5],
+        ['Juana', 19.415,  -99.175, 'movil', 1, 7, 7, 4, 0]
+      ]; 
+
+      var nombreT = "Jesús Ramírez";
+      var cantidadDulce =0, cantidadVerde = 0, cantidadMole=0, cantidadRajas=0, cantidadOaxaqueño=0;
+      var tamaleroY=19.415, tamaleroX=-99.170;
+
+      var tamalero = {
+        nombre: nombreT,
+        dulce: cantidadDulce,
+        verde: cantidadVerde,
+        mole: cantidadMole,
+        rajas: cantidadRajas,
+        oaxaqueño: cantidadOaxaqueño,
+        y: tamaleroY,
+        x: tamaleroX
+      }
+
+function setMarkers(map, locations) {
+
+  var image = {
+    url: 'images/fijo-icon.png',
+    // This marker is 20 pixels wide by 32 pixels tall.
+    size: new google.maps.Size(32, 32),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(0, 32)
+  };
+  // Shapes define the clickable region of the icon.
+  // The type defines an HTML &lt;area&gt; element 'poly' which
+  // traces out a polygon as a series of X,Y points. The final
+  // coordinate closes the poly by connecting to the first
+  // coordinate.
+  var shape = {
+      coords: [1, 1, 1, 34, 34, 34, 34 , 1],
+      type: 'poly'
+  };
+  for (var i = 0; i < locations.length; i++) {
+    var beach = locations[i];
+    var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image,
+        shape: shape,
+        title: beach[0],
+    });
+
+    var contentString = '<div class="modal-header">'+
+        '<h4 class="modal-title" id="myModalLabel">'+ tamalero.nombre +'</h4>'+
+        '<h4 class="modal-title" id="distancia">Distancia aproximada: 4km</h4>'+
+      '</div>'+
+      '<div class="modal-body">'+
+        '<div class="orden center clearfix">'+
+          '<table id = "tabla">'+
+            '<p>Mi inventario</p>'+
+            '<tr id="tablaDulce">'+
+              '<td class="sabor">Dulce</td>'+
+              '<td class="cantidad">'+tamalero.dulce+'</td>'+   
+            '</tr>'+
+
+            '<tr id="Verde">'+
+              '<td class="sabor">Verde</td>'+
+              '<td class="cantidad">'+ tamalero.verde+'</td>'+   
+            '</tr>'+
+            '<tr id="Mole">'+
+              '<td class="sabor">Mole</td>'+
+              '<td class="cantidad">'+ tamalero.mole+'</td>'+   
+            '</tr>'+
+            '<tr id="Rajas">'+
+              '<td class="sabor">Rajas</td>'+
+              '<td class="cantidad">'+ tamalero.rajas+'</td>'+   
+            '</tr>'+
+            '<tr id="Oaxaqueño">'+
+              '<td class="sabor">Oaxaqueño</td>'+
+              '<td class="cantidad">'+ tamalero.oaxaqueño+'</td>'+   
+            '</tr>'+
+          '</table>'+
+        '</div><!--INVENTARIO-->'+
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200
+  });
+
+    google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+  }
+}
+
+
+
+      function geolocationSuccess(position) {
+        var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+        var myOptions = {
+          zoom : 16,
+          center : userLatLng, //disableDefaultUI: true
+          mapTypeId : google.maps.MapTypeId.ROADMAP
+        };
+        // Draw the map
+        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+        
+        setMarkers(mapObject, beaches);
+
+        // Place the marker
+        new google.maps.Marker({
+          map: mapObject,
+          position: userLatLng
+        });
+      }
+ 
+      function geolocationError(positionError) {
+        document.getElementById("error").innerHTML += "Error: " + positionError.message + "<br />";
+      }
+ 
+      function geolocateUser() {
+        // If the browser supports the Geolocation API
+        if (navigator.geolocation)
+        {
+          var positionOptions = {
+            enableHighAccuracy: true,
+            timeout: 10 * 1000 // 10 seconds
+          };
+          navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+        }
+        else
+          document.getElementById("error").innerHTML += "Your browser doesn't support the Geolocation API";
+      }
